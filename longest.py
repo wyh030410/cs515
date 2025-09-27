19985fcce73f5979b3e0ed3c874d58d61c5c509a
wyh030410 <wangyuhui030410@gmail.com>
2025-09-26 23:57:17 -0400

    1

--- longest.py
+++ longest.py
@@ -3,7 +3,3 @@ def longest_lines(filename):
     lines = [line.rstrip('\n') for line in lines]
     max_length = max(len(line) for line in lines)
     return [line for line in lines if len(line) == max_length]
-with open('foo.txt', 'w') as f:
-    f.write('a\nb\ncd\nef\ng\n')
-
-print(longest_lines('foo.txt'))
